491daa859ae4f7c00384261e12d6e02e9b271d11
Payam Hoseini <p.hoseini.ch@gmail.com>
2024-04-05 19:42:23 +0330

    Make song and player components interactive.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles/app.scss";
+import data from "./data";
 
 //Import Components
 import Player from "./Components/Player";
 import Song from "./Components/Song";
 
 function App() {
+  //States
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+
   return (
     <div className="App">
-      <Song />
-      <Player />
+      <Song currentSong={currentSong} />
+      <Player
+        setIsPlaying={setIsPlaying}
+        currentSong={currentSong}
+        songs={songs}
+        isPlaying={isPlaying}
+      />
     </div>
   );
 }
