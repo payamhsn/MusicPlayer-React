c201877d9f9dbd662677c3552585044a3b58b332
Payam Hoseini <p.hoseini.ch@gmail.com>
2024-04-05 15:38:14 +0330

    Create two components. Install FontAwsome for use icons.

--- src/App.js
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div className="App">Hello Payam</div>;
-}
-
-export default App;
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+//Import Components
+import Player from "./Components/Player";
+import Song from "./Components/Song";
+
+function App() {
+  return (
+    <div className="App">
+      <Song />
+      <Player />
+    </div>
+  );
+}
+
+export default App;
